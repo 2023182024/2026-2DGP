1b8d91b2fa3ba152d4189e43ec0c05f23987d8f6
osm425 <osm040425@gmail.com>
2026-09-22 22:24:27 +0900

    사각 움직임 구현

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,16 +1,31 @@
 from pico2d import *
-
 open_canvas(800, 600)
+
+x = 300
+y = 200
 
 character = load_image('character.png')
 
 def move_circle():
+    clear_canvas()
+
     pass
 
 def move_rectangle():
+    clear_canvas()
+    if x<500 and y==200:
+        x += 2
+    elif x==500 and y<400:
+        y += 2
+    elif x>300 and y==400:
+        x -= 2
+    elif x==300 and y>200:
+        y -= 2
+    character.draw(x, y)
     pass
 
 def move_triangle():
+    clear_canvas()
     pass
 
 
