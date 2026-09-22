c7d05c26373a8fe89b2cff867b0148decbbe472f
osm425 <osm040425@gmail.com>
2026-09-22 22:57:44 +0900

    원운동 이후 character 위치 조정

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -7,32 +7,42 @@ y = 200
 character = load_image('character.png')
 
 def move_circle(x,y):
-    clear_canvas()
-
+    radius = 200
+    angle = 0
+    while angle < 2 * 3.14:
+        clear_canvas()
+        x = 400 + radius * math.cos(angle)
+        y = 300 + radius * math.sin(angle)
+        character.draw(x, y)
+        angle += 0.1
+        print(x,y,angle)
+        delay(0.05)
+    x=300
+    y=200
     pass
 
 def move_rectangle(x,y):
     while x < 800:
         clear_canvas()
-        x+=2
+        x+=5
         print(x, y)
         character.draw(x, y)
         delay(0.01)
     while y < 600:
         clear_canvas()
-        y+=2
+        y+=5
         print(x, y) 
         character.draw(x, y)
         delay(0.01)
     while x > 0:
         clear_canvas()
-        x-=2
+        x-=5
         print(x, y)
         character.draw(x, y)
         delay(0.01)
     while y > 0:
         clear_canvas()
-        y-=2
+        y-=5
         print(x, y)
         character.draw(x, y)
         delay(0.01)
